70334ef5aaa8138d187d76035485298c3ccdb7b1
himel2535 <monwarhossanhimel@gmail.com>
2025-11-03 15:32:17 +0600

    env variable

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.port || 3000;
 
+
 // ---middleware---
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://smartdbUser:6UeDdOdDJZSPjQAZ@himelcluster.fxzuftr.mongodb.net/?appName=HimelCluster";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@himelcluster.fxzuftr.mongodb.net/?appName=HimelCluster`;
+
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -31,6 +33,25 @@ async function run() {
     const db = client.db("smart_db");
     const productsCollection = db.collection("products");
     const bidsCollection = db.collection("bids");
+    const usersCollection = db.collection("users");
+
+    // ----Post User----
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+
+      const email = req.body.email;
+      const query = { email: email };
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        res.send({
+          message: "user already exist. do not need to insert again",
+        });
+      } else {
+        const result = await usersCollection.insertOne(newUser);
+        res.send(result);
+      }
+    });
 
     // ----Get All Products----
     app.get("/products", async (req, res) => {
@@ -54,11 +75,40 @@ async function run() {
       res.send(result);
     });
 
-    // ----Get One Products----
+    // ----Get One Product----
+
     app.get("/products/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await productsCollection.findOne(query);
+      try {
+        const id = req.params.id;
+        const query = { _id: id };
+        const result = await productsCollection.findOne(query);
+
+        if (result) {
+          res.send(result);
+        } else {
+          res.status(404).json({ message: "Product not found" });
+        }
+      } catch (error) {
+        console.error("Error fetching product:", error);
+        res.status(400).json({ message: "Invalid product ID format" });
+      }
+    });
+
+    // ---Get Recent Product---
+    app.get("/latest-products", async (req, res) => {
+      const projectFields = {
+        title: 1,
+        price_min: 1,
+        price_max: 1,
+        image: 1,
+        condition: 1,
+      };
+      const cursor = productsCollection
+        .find()
+        .sort({ created_at: -1 })
+        .limit(6)
+        .project(projectFields);
+      const result = await cursor.toArray();
       res.send(result);
     });
 
@@ -71,7 +121,7 @@ async function run() {
 
     // ----Update Products----
     app.patch("/products/:id", async (req, res) => {
-      const id = req.params;
+      const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       updatedProduct = req.body;
       const update = {
@@ -104,10 +154,28 @@ async function run() {
       res.send(result);
     });
 
+    // ----Get One Bid by Product---
+    app.get("/products/bids/:productId", async (req, res) => {
+      const productId = req.params.productId;
+      const query = { product: productId };
+      const cursor = bidsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // -----Create Bid-----
     app.post("/bids", async (req, res) => {
       const newBid = req.body;
+
       const result = await bidsCollection.insertOne(newBid);
+      res.send(result);
+    });
+
+    // ----Delete Bid----
+    app.delete("/bids/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bidsCollection.deleteOne(query);
       res.send(result);
     });
 
